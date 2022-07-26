6c45a3d913b274c318865bb2892c7632c19501b2
Farhad <ifarhadfaghihi@gmail.com>
2022-07-26 15:11:19 -0600

    initial commit

---
+++ foodbox-api/src/test/java/food/box/FoodboxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package food.box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodboxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
