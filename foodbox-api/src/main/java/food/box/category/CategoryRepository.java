da179230cba72c6c2f71bf4d70f225a55b174eae
Farhad <ifarhadfaghihi@gmail.com>
2022-08-20 10:01:10 -0600

    added crossorigin to data rest apis

--- foodbox-api/src/main/java/food/box/category/CategoryRepository.java
+++ foodbox-api/src/main/java/food/box/category/CategoryRepository.java
@@ -2,7 +2,9 @@ package food.box.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "category", path = "category")
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
--- foodbox-api/src/main/java/food/box/dish/DishRepository.java
+++ foodbox-api/src/main/java/food/box/dish/DishRepository.java
@@ -3,9 +3,11 @@ package food.box.dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "dish", path = "dish")
 public interface DishRepository extends JpaRepository<Dish, Long> {
 
